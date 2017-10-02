b85ab63e2055b3efdf5086727c3770f113d77e95
Joao Costa <joaocosta8889@outlook.com>
2017-10-02 08:00:45 +0100

    Simple code for display message "hello world" using MessageBox of Windows.Forms.

---
+++ HelloWord/HelloWorldWindowsForms/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace HelloWorldWindowsForms
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            MessageBox.Show("Hello world!!","Message", MessageBoxButtons.OK);
+        }
+    }
+}
